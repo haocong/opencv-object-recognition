9800d5cf7a55c84dda91356c43fd83e66d29b8ce
haocong <haocong.xu@gmail.com>
2017-01-03 17:24:49 +0800

    Initial Commit

---
+++ MyFirstOpenCV/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MyFirstOpenCV
+//
+//  Created by haocong on 2017/1/3.
+//  Copyright © 2017年 nankai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
